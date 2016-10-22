060cd27843ad624e308635ee1357346763d551c5
Wanasak <u510610433@gmail.com>
2016-10-23 00:05:10 +0700

    commit
    - We added the necessary dependencies to use HTTP in our application
    - We refactored HeroService to load hero from a web api
    - We extened HeroService to support post, put and delete method
    - We updated our components to allow adding, editing and deleting of
    heroes
    - We configured an in-memory web api
    - We learned how to use Observables

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,32 +1,35 @@
+import './rxjs-extensions';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http'; // import http module
 import { FormsModule } from '@angular/forms'; // use two way binding for from inputs
 //import { RouterModule } from '@angular/router';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component'; // import dashboard component
 import { HeroesComponent } from './heroes.component'; // import heroes component
 import { HeroDetailComponent } from './hero-detail.component'; // import hero detail component
+import { HeroSearchComponent } from './hero-search.component';
 import { HeroService } from './hero.service'; // import hero service
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [ 
     BrowserModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     FormsModule, // add forms module
-    AppRoutingModule
-    // RouterModule.forRoot([
-    //   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    //   { path: 'heroes', component: HeroesComponent},
-    //   { path: 'detail/:id', component: HeroDetailComponent },
-    //   { path: 'dashboard', component: DashboardComponent}
-    // ])
+    AppRoutingModule // add routing module
   ],
   declarations: [ 
     AppComponent,
     DashboardComponent, // add dashboard component
     HeroesComponent, // add heroes component
-    HeroDetailComponent // add hero detail component
+    HeroDetailComponent ,// add hero detail component
+    HeroSearchComponent
   ],
   providers: [
     HeroService // add hero service
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -8,17 +8,7 @@ import { HeroService } from './hero.service'; // import hero service
 @Component({
     moduleId: module.id,
     selector: 'my-hero-detail',
-    template: `
-        <div *ngIf="hero">
-            <h2>{{ hero.name }} details!</h2>
-            <div><label>id: </label>{{ hero.id }}</div>
-            <div>
-                <label>name: </label>
-                <input placeholder="name" [(ngModel)]="hero.name" />
-            </div>
-        </div>
-        <button (click)="goBack()">Back</button>
-    `,
+    templateUrl: 'hero-detail.component.html',
     styleUrls: [ 'hero-detail.component.css' ]
 })
 
@@ -40,6 +30,11 @@ export class HeroDetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
     }
     
     @Input() // annotation input
